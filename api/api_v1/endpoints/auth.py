26a11be6950669c9ca1b524c984b7bb38cc97b0c
AijazAhmedBZ <aijaz.ahmed.bz@gmail.com>
2024-01-11 07:35:45 +0500

    updated on 11-Jan-24

--- api/api_v1/endpoints/auth.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from security import oauth2_scheme
-
-router = APIRouter()
-
-@router.post("/token")
-def login(username: str, password: str):
-    # Implementation of token generation (JWT)
-    pass
--- api/crud.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import Session
-from models import Todo
-
-def create_todo(db: Session, title: str, description: str, owner_id: int):
-    db_todo = Todo(title=title, description=description, owner_id=owner_id)
-    db.add(db_todo)
-    db.commit()
-    db.refresh(db_todo)
-    return db_todo
